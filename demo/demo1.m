39f79bf26bdb23d4d34c79167a42a12ad7333f00
DianaDeng <37362495+DianaDeng@users.noreply.github.com>
2019-10-09 15:16:48 +0800

    Add files via upload

--- demo/demo1.m
+++ demo/demo1.m
@@ -11,11 +11,10 @@ Y = [2;1;2;1;2;1;2;1;1;2];
 %for i=1:8
 %    lambda(i)=a^(-6+i);
 %end
-lambda=linspace(0.06,0.15,10);
+lambda=linspace(0.001,0.015,15);
 
-%l = wmaxlev(128,'haar'); %get maximum possible decomposition level
-%level=linspace(1,l,l);
-level=1;
+l = wmaxlev(128,'haar'); %get maximum possible decomposition level
+level=linspace(1,l,l);
 
 %Obtain C matrix at specified levels
 [C_levels,size_X] = getCmatrix(X,level,1);
@@ -23,9 +22,10 @@ level=1;
 %Apply the cross validation to select optimal set of tuning parameters
 %result =hwrfr_cv(level,lambda,5,C_levels, Y,1);
 result =hwrfr_cv(level,lambda,2,C_levels, Y,2);
-
-%Obtain beta by eta
-beta_est= getbeta(result.eta_auc,result.level_auc,1,size_X);
 %draw ROC plot
 plot_best_roc(C_levels{result.level_auc},result.int_auc,result.eta_auc,Y);
+
+%Obtain beta by eta
+%beta_est= getbeta(result.eta_auc,result.level_auc,1,size_X);
+
 
